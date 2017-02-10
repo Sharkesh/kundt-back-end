1cc65aaf13df07ade6c95540ddc9429cea8533a8
Mario Affenzeller <mario-affenzeller@gmx.at>
2017-02-10 08:12:45 +0100

    Stept "KundenUebersicht" erstes Layout
    bla

--- kundt-back-end/Controllers/HomeController.cs
+++ kundt-back-end/Controllers/HomeController.cs
@@ -12,6 +12,9 @@ namespace kundt_back_end.Controllers
         {
             return View();
         }
-        
+        public ActionResult UebersichtKunden()
+        {
+            return View();
+        }
     }
 }
