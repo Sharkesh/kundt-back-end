b6439e95be87dbc130dc5bd975c07eadb7cd5673
Dominic Rakousky <dominic.rakousky@chello.at>
2017-02-16 15:27:59 +0100

    Added Location Highlighting

---
+++ kundt-back-end/Scripts/scripts.js
@@ -0,0 +1,27 @@
+﻿function Active() {
+    if (document.title == "Übersicht") {
+        $("#uebersicht").addClass("active");
+    } else {
+        $("#uebersicht").removeClass("active");
+    }
+    if (document.title == "Übersicht Autos" || document.title == "Auto Hinzufügen") {
+        $("#fahrzeug").addClass("active");
+    } else {
+        $("#fahrzeug").removeClass("active");
+    }
+    if (document.title == "Übersicht Kunden" || document.title == "Kunde Hinzufügen") {
+        $("#kunden").addClass("active");
+    } else {
+        $("#kunden").removeClass("active");
+    }
+    if (document.title == "Übersicht Buchungen") {
+        $("#buchung").addClass("active");
+    } else {
+        $("#buchung").removeClass("active");
+    }
+    if (document.title == "Übersicht Mitarbeiter" || document.title == "Mitarbeiter Hinzufügen") {
+        $("#mitarbeiter").addClass("active");
+    } else {
+        $("#mitarbeiter").removeClass("active");
+    }
+}
