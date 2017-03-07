190ddc1d3e758472e0fb0ba9c8e9061ec63fe9f7
Christian Schieder <christian.schieder@hotmail.com>
2017-03-07 16:29:22 +0100

    update
    Index update

--- kundt-back-end/Controllers/BuchungUebersichtController.cs
+++ kundt-back-end/Controllers/BuchungUebersichtController.cs
@@ -17,32 +17,17 @@ namespace kundt_back_end.Controllers
         // GET: BuchungUebersicht
         public ActionResult Index()
         {
-            var buchungAbholung = db.tblBuchung.Include(b => b.tblAuto).Include(b => b.tblKunde).Where(b => b.BuchungVon == DateTime.Now);
+            var buchungAbholung = db.tblBuchung.Include(b => b.tblAuto).Include(b => b.tblKunde).Where(b => b.BuchungVon == DateTime.Today && !b.Storno);
 
-            var buchungRueckgabe = db.tblBuchung.Include(b => b.tblAuto).Include(b => b.tblKunde).Where(b => b.BuchungBis == DateTime.Now);
+            var buchungRueckgabe = db.tblBuchung.Include(b => b.tblAuto).Include(b => b.tblKunde).Where(b => b.BuchungBis == DateTime.Today && b.BuchungStatus == "abgeholt");
 
-            var buchungProblem = db.tblBuchung.Include(b => b.tblHistorie).Where(b => (b.BuchungVon < DateTime.Now && b.BuchungStatus == "erstellt") || (b.BuchungBis < DateTime.Now && b.BuchungStatus == "abgeholt"));
+            var buchungProblem = db.tblBuchung.Include(b => b.tblHistorie).Where(b => (b.BuchungVon < DateTime.Today && b.BuchungStatus == "erstellt") || (b.BuchungBis < DateTime.Today && b.BuchungStatus == "abgeholt"));
 
             BuchungsUebersicht bu = new BuchungsUebersicht();
             bu.buchungAbholung = buchungAbholung.ToList();
             bu.buchungRueckgabe = buchungRueckgabe.ToList();
             bu.buchungProblem = buchungProblem.ToList();
             return View(bu);
-        }
-
-        // GET: BuchungUebersicht/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblBuchung tblBuchung = db.tblBuchung.Find(id);
-            if (tblBuchung == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tblBuchung);
         }
 
         // GET: BuchungUebersicht/Edit/5
