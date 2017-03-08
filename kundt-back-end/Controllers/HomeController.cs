e7987bab00fa28c17e3ae21058f61f6770ac386a
Mario Affenzeller <mario-affenzeller@gmx.at>
2017-03-08 09:59:21 +0100

    Übersicht Kunden Dynamik hinzugefüght
    HomeController veränderungen
    
    HTML Kundenübersicht alles reinladen

--- kundt-back-end/Controllers/HomeController.cs
+++ kundt-back-end/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using kundt_back_end.Models;
 
 namespace kundt_back_end.Controllers
 {
@@ -12,10 +15,19 @@ namespace kundt_back_end.Controllers
         {
             return View();
         }
+        // it22Autoverlei mit namen DB ist eine neue blaaaa eh scho wissen. Wichtige zeile damit die weiterleitung
+        // funtzt.
+        private it22AutoverleihEntities db = new it22AutoverleihEntities();
+
         public ActionResult KundenUebersicht()
         {
-            return View();
+            // die variable "tblKunde" enthält die daten aus der Tabelle Kunde/ort/Login
+            var tblKunde = db.tblKunde.Include(t => t.tblPLZOrt);
+            // Schmeis dem View die Liste mit allen Daten aus der Variable "tblKunde" ins Gsicht!
+            return View(tblKunde.ToList());
         }
+
+
         public ActionResult KundenDetail()
         {
             return View();
