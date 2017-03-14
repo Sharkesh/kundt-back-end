84fad0673f7d9f383260c7e8530ca912c33e5ea9
Christian Schieder <christian.schieder@hotmail.com>
2017-03-14 16:31:15 +0100

    update
    neues Model für BuchungEdit angelegt
    BuchungEdit View angepasst und formatiert

---
+++ kundt-back-end/Models/BuchungEditModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace kundt_back_end.Models
+{
+    public class BuchungEditModel
+    {
+        public int IDBuchung { get; set; }        
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public System.DateTime BuchungVon { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public System.DateTime BuchungBis { get; set; }
+        public bool Versicherung { get; set; }
+        public int FKKunde { get; set; }
+        public int FKAuto { get; set; }
+        public string BuchungStatus { get; set; }
+        public bool Storno { get; set; }
+    }
+}
