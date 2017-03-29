8435c6c6c59a3ce1bb364d99b28d0b5e6e17c037
Dark Hindi <dark.hindi@gmx.at>
2017-03-29 13:55:47 +0200

    Conflict fehler behoben

--- kundt-back-end/Models/It22AutoverleihModel.Designer.cs
+++ kundt-back-end/Models/It22AutoverleihModel.Designer.cs
@@ -1,6 +1,4 @@
-﻿
-﻿// Die T4-Codegenerierung ist für Modell 'C:\Users\kreimich\Documents\GitHub\kundt-back-end\kundt-back-end\Models\It22AutoverleihModel.edmx' aktiviert. 
-
+﻿// Die T4-Codegenerierung ist für Modell 'C:\Users\MIlijana\Documents\GitHub\kundt-back-end\kundt-back-end\Models\It22AutoverleihModel.edmx' aktiviert. 
 // Um die Generierung von Legacycode zu aktivieren, ändern Sie den Wert der Designer-Eigenschaft 'Code Generation Strategy'
 // in 'Legacy ObjectContext'. Diese Eigenschaft wird im Eigenschaftenfenster angezeigt, wenn das Modell
 // im Designer geöffnet ist.
